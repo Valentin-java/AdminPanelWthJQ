daaf75f7d661785f77ee86854ce90b90eb733841
Valentin-java <v_tin@mail.ru>
2021-10-24 19:23:31 +0300

    1.6 (with UI)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
 
-    $('.table .eBtn').on('click',function (event) {
+    $('.nBtn, .table .eBtn').on('click',function (event) {
         event.preventDefault();
         var href = $(this).attr('href');
+        var text = $(this).text();
+
+        if (text == 'Edit') {
 
         $.get(href,function (user, status) {
-            $('.myForm #id').val(user.id);
+
             $('.myForm #username').val(user.username);
             $('.myForm #age').val(user.age);
             $('.myForm #email').val(user.email);
             $('.myForm #password').val(user.password);
             $('.myForm #confirmPassword').val(user.password);
         });
-
-
         $('.myForm #EditModal').modal();
+    } else {
+
+            $('.myForm #username').val('');
+            $('.myForm #age').val('');
+            $('.myForm #email').val('');
+            $('.myForm #password').val('');
+            $('.myForm #confirmPassword').val('');
+            $('.myForm #EditModal').modal();
+    }
     });
 });
+
